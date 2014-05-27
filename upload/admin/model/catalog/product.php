691626db01b54bba2c0ac932644b909559203ec1
Sebastian Gonzalez Riffo <sebastian@gonzalezr.cl>
2014-05-27 16:46:48 -0300

    Tab Option Color-Talla

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -508,6 +508,47 @@ class ModelCatalogProduct extends Model {
 		return $product_option_data;
 	}
 
+	public function getProductOptionsCT($product_id) {
+		$product_option_data = array();
+
+		$sql = "SELECT * FROM `" . DB_PREFIX . "product_option` po LEFT JOIN `" . DB_PREFIX . "option` o ON (po.option_id = o.option_id) LEFT JOIN `" . DB_PREFIX . "option_description` od ON (o.option_id = od.option_id) WHERE po.product_id = '" . (int)$product_id . "' AND od.language_id = '" . (int)$this->config->get('config_language_id') . "' AND o.`type` = 'select_ct'";
+
+		$product_option_query = $this->db->query($sql);
+
+		foreach ($product_option_query->rows as $product_option) {
+			$product_option_value_data = array();	
+
+			$product_option_value_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_option_value WHERE product_option_id = '" . (int)$product_option['product_option_id'] . "'");
+
+			foreach ($product_option_value_query->rows as $product_option_value) {
+				$product_option_value_data[] = array(
+					'product_option_value_id' => $product_option_value['product_option_value_id'],
+					'option_value_id'         => $product_option_value['option_value_id'],
+					'quantity'                => $product_option_value['quantity'],
+					'subtract'                => $product_option_value['subtract'],
+					'price'                   => $product_option_value['price'],
+					'price_prefix'            => $product_option_value['price_prefix'],
+					'points'                  => $product_option_value['points'],
+					'points_prefix'           => $product_option_value['points_prefix'],						
+					'weight'                  => $product_option_value['weight'],
+					'weight_prefix'           => $product_option_value['weight_prefix']					
+				);
+			}
+
+			$product_option_data[] = array(
+				'product_option_id'    => $product_option['product_option_id'],
+				'option_id'            => $product_option['option_id'],
+				'name'                 => $product_option['name'],
+				'type'                 => $product_option['type'],			
+				'product_option_value' => $product_option_value_data,
+				'option_value'         => $product_option['option_value'],
+				'required'             => $product_option['required']				
+			);
+		}
+
+		return $product_option_data;
+	}
+
 	public function getProductImages($product_id) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_image WHERE product_id = '" . (int)$product_id . "'");
 
