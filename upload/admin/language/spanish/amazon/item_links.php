c66cd7eedb3220459724258774f01eadcc9dc445
Sebastian Gonzalez Riffo <sebastian@gonzalezr.cl>
2014-05-29 01:20:34 -0300

    Lenguaje en español

---
+++ upload/admin/language/spanish/amazon/item_links.php
@@ -0,0 +1,27 @@
+<?php
+$_['lang_title']                    = 'OpenBay Pro para Amazon | Enlaces de Artículo';
+$_['lang_openbay']                  = 'OpenBay Pro';
+$_['lang_overview']                 = 'Estado Amazon';
+$_['lang_btn_return']               = 'Devolver';
+$_['lang_link_items']               = 'Enlace a Artículos';
+$_['lang_item_links']               = 'Enlaces de Artículo';
+$_['lang_desc1']                    = 'Enlazar tus Artículos te permitira controlar tu Inventario en Listados de Amazon.<br /> Para cada Artículo que es actualizado en el Inventario local (el Inventario disponible en tu Tienda) actualizara tu Listado de Amazon';
+$_['lang_desc2']                    = 'Puedes enlanzar Artículos manualmente introduciendo el SKU de Amazon y nombre de Producto o cargar todos los Productos no enlazados y luego introducir el SKU de Amazon. (Cargar Productos desde OpenCart a Amazon agregara automaticamente Enlaces)';
+$_['lang_load_btn']                 = 'Cargar';
+$_['lang_new_link']                 = 'Nuevo Enlace';
+$_['lang_autocomplete_product']     = 'Producto <span class="help">(Autocompletar desde Nombre)</span>';
+$_['lang_amazon_sku']               = 'SKU de Artículo en Amazon';
+$_['lang_action']                   = 'Modificar';
+$_['lang_add']                      = 'Agregar';
+$_['lang_add_sku_tooltip']          = 'Agregar otro SKU';
+$_['lang_remove']                   = 'Remover';
+$_['lang_linked_items']             = 'Artículos Enlazados';
+$_['lang_unlinked_items']           = 'Artículos Desenlazados';
+$_['lang_name']                     = 'Nombre';
+$_['lang_model']                    = 'Modelo';
+$_['lang_combination']              = 'Combinación';
+$_['lang_sku']                      = 'SKU';
+$_['lang_amazon_sku']               = 'SKU de Artículo en Amazon';
+$_['lang_sku_empty_warning']        = 'Amazon SKU no puede estar vacío!';
+$_['lang_name_empty_warning']       = 'Nombre de Producto no puede estar vacío!';
+$_['lang_product_warning']          = 'Producto no existe. Favor de usar valores de autocompletación';
